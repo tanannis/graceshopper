022e7ffc95b76c21017d9394ca5761fded96bb45
Annie Ilizarov <annie.ilizarov@gmail.com>
2020-11-12 15:06:42 -0500

    added email test spec to user model

--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -43,75 +43,23 @@ describe('User model', () => {
         expect(user.firstName).to.equal('Sally')
         expect(user.lastName).to.equal('Ride')
         expect(user.email).to.equal('sallyride@nasa.gov')
+        //README - do we want to test if password is a function?
         //     expect(user.password).to.equal(get() {
         //   return () => this.getDataValue('password')
         // })
         expect(user.userType).to.equal('user')
       })
+
+      it('email must be a valid email', async () => {
+        const user = User.build({
+          email: 'figgy'
+        })
+        try {
+          await user.validate()
+        } catch (err) {
+          expect(err.message).to.contain('Validation isEmail on email failed')
+        }
+      })
     }) // end describe('correctPassword')
   }) // end describe('instanceMethods')
 }) // end describe('User model')
-
-xdescribe('Sequelize Model', () => {
-  before(() => db.sync({force: true}))
-  afterEach(() => db.sync({force: true}))
-
-  it('has fields firstName, lastName, email, passsword, userType', async () => {
-    const user = await User.create({
-      firstName: 'Sally',
-      lastName: 'Ride',
-      email: 'sallyride@nasa.gov',
-      password: 'hello',
-      userType: 'user'
-    })
-    expect(user.firstName).to.equal('Sally')
-    expect(user.lastName).to.equal('Ride')
-    expect(user.email).to.equal('sallyride@nasa.gov')
-    expect(user.password).to.equal('hello')
-    expect(user.userType).to.equal('user')
-  })
-
-  it('requires firstName, lastName, email', async () => {
-    const user = User.build()
-    try {
-      await user.validate()
-      throw Error(
-        'validation should have failed without firstName, lastName, email'
-      )
-    } catch (err) {
-      expect(err.message).to.contain('firstName cannot be null')
-      expect(err.message).to.contain('lastName cannot be null')
-      expect(err.message).to.contain('email cannot be null')
-      expect(err.message).to.contain('password cannot be null')
-    }
-  })
-
-  it('firstName, lastName, email, password cannot be empty', async () => {
-    const user = User.build({
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: ''
-    })
-    try {
-      await user.validate()
-      throw Error('validation should have failed with empty name and address')
-    } catch (err) {
-      expect(err.message).to.contain('Validation notEmpty on firstName')
-      expect(err.message).to.contain('Validation notEmpty on lastName')
-      expect(err.message).to.contain('Validation notEmpty on email')
-      expect(err.message).to.contain('Validation notEmpty on password')
-    }
-  })
-
-  it('email must be a valid email', async () => {
-    const user = User.build({
-      email: 'figgy'
-    })
-    try {
-      await user.validate()
-    } catch (err) {
-      expect(err.message).to.contain('Validation isEmail on email failed')
-    }
-  })
-})
