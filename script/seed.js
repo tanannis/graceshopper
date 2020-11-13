bf3189d8f4acc870df3dcaf79544e1ea727e3efc
tanannis <64669206+tanannis@users.noreply.github.com>
2020-11-12 22:25:51 -0500

    Updated seed file

--- script/seed.js
+++ script/seed.js
@@ -39,7 +39,8 @@ const productsArray = [
     name: 'Fruit Napoleon',
     imageUrl:
       'https://i.ibb.co/ygz1yX6/The-Best-Napoleon-Cake-is-made-with-thin-puff-pastry-layers-then-sandwiched-with-rich-and-buttery-cu.jpg',
-    description: 'Less than 200 cal',
+    description:
+      'Our signature handmade napoleon made of very thin and flaky puff pastry cake layers and a smooth, rich and luscious pastry cream in between the layers, topped with fresh fruit.',
     price: 8,
     quantity: 10
   },
@@ -47,21 +48,24 @@ const productsArray = [
     name: 'Chocolate Dream Donuts',
     imageUrl:
       'https://i.ibb.co/yQ3XTrZ/gluten-free-vegan-baked-chocolate-donuts.jpg',
-    description: 'Less than 300 cal',
+    description:
+      'Our famous chocolate donuts covered with silky melted chocolate glaze, paradise in every bite...',
     price: 3,
     quantity: 10
   },
   {
     name: 'Pear Puff',
     imageUrl: 'https://i.ibb.co/Bc13Pnz/poached-pear-puff-pastry-1.jpg',
-    description: 'Less than 100 cal',
+    description:
+      'Limited edition: pear puff made of very thin and flaky puff pastry cake layers and a smooth, rich and luscious pear pastry cream in between the layers.',
     price: 6,
     quantity: 10
   },
   {
     name: 'French Croissant',
     imageUrl: 'https://i.ibb.co/5KvXzfn/Italian-croissants-15-500x500.jpg',
-    description: 'Less than 300 cal',
+    description:
+      'Our best seller: this handmade cloud-like puff pastry is buttery and flaky. It really is one of the most decadent things you can eat.',
     price: 3,
     quantity: 10
   },
@@ -69,56 +73,64 @@ const productsArray = [
     name: 'Cream Puff',
     imageUrl:
       'https://i.ibb.co/RgZSP1W/cream-puff-recipe-chocolate-glaze-1-S-12750.jpg',
-    description: 'Less than 200 cal',
+    description:
+      'Try our new fluffy cream puff today! It is made of very thin and flaky puff pastry layers filled with rich vanilla cream inside.',
     price: 6,
     quantity: 10
   },
   {
     name: 'Fruit Tart',
     imageUrl: 'https://i.ibb.co/gVRWrbY/Fruit-tart-2019-feature.jpg',
-    description: 'Less than 200 cal',
+    description:
+      'This fruit tart has a sweet pastry crust, smooth cream filling and topped with organic fresh fruit. It is a healthy and elegant dessert for tea time!',
     price: 6,
     quantity: 10
   },
   {
     name: 'New York Cheese Cake',
     imageUrl: 'https://i.ibb.co/5sWXFvq/Classic-Cheesecake-Recipe-1.jpg',
-    description: 'Less than 400 cal',
+    description:
+      'Creamy and delicious with a touch of lime juice, the New York Cheese Cake is one of our best sellers!',
     price: 7,
     quantity: 10
   },
   {
     name: 'Strawberry Cheese Cake',
     imageUrl: 'https://i.ibb.co/JW1r37d/mini-cheesecakes-21.jpg',
-    description: 'Less than 300 cal',
+    description:
+      'Topped with fresh organic strawberry, this creamy cheese cake is our new signature for an elegant experience.',
     price: 6,
     quantity: 10
   },
   {
     name: 'Curry Triangle Puff',
     imageUrl: 'https://i.ibb.co/vcf2NJs/Puff-Pastry-Pockets1.jpg',
-    description: 'Less than 200 cal',
+    description:
+      'A classic Asian pastry with curry filling inside, its crust is made of very thin and flaky puff pastry cake layers that will give you an unique experience. ',
     price: 6,
     quantity: 10
   },
   {
     name: 'Mini Spinach Quiche',
     imageUrl: 'https://i.ibb.co/vQbh93b/spinach-cheese-puffs-4.jpg',
-    description: 'Less than 400 cal',
+    description:
+      'Our new favorite: made with heavy cream and organic spinach, this quiche is a classic French pastry.',
     price: 5,
     quantity: 10
   },
   {
     name: 'Egg Tart',
     imageUrl: 'https://i.ibb.co/fkXB6FV/Hokkaido-Cheese-Tart-2.jpg',
-    description: 'Less than 300 cal',
+    description:
+      'Our new favorite: made with organic eggs, its crust is made of flaky puff pastry cake layers, this tart is a classic Asian pastry.',
     price: 5,
     quantity: 10
   },
   {
     name: 'Chocolate Chip Scone',
     imageUrl: 'https://i.ibb.co/PGVJkcq/Easy-Chocolate-Chip-Scones.jpg',
-    description: 'Less than 500 cal',
+    description:
+      'Our signature scone: it has a “short” crumbly texture that is a must for afternoon tea!',
     price: 5,
     quantity: 10
   }
