65f9e584a325d6123af55151eed71aec55853a49
Annis Tan <tanannis@gmail.com>
2020-11-14 12:20:19 -0500

    Checked something

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import AllUsers from './AllUsers'
+import AllProducts from './AllProducts'
 
 /**
  * COMPONENT
