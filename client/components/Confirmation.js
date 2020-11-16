a8d3914a68f65f754e61ecaf5aa5f05a5ddb0a0c
Laura <lmmcampbell@gmail.com>
2020-11-16 17:05:56 -0600

    css udpates

--- client/components/Confirmation.js
+++ client/components/Confirmation.js
@@ -4,8 +4,16 @@ export default class Confirmation extends React.Component {
   render() {
     return (
       <div className="thankYou">
-        <h2>Thank you for your order! We hope you enjoy your treats!</h2>
-        <h2>Come back soon!</h2>
+        <h2 className="thankYouContent">
+          Thank you for your order! We hope you enjoy your treats!
+        </h2>
+        <h2 className="thankYouContent">Come back soon!</h2>
+        <div className="thankYouContent">
+          <img
+            id="forkImage"
+            src="https://cdn.pixabay.com/photo/2013/07/12/15/07/fork-149488_1280.png"
+          />
+        </div>
       </div>
     )
   }
